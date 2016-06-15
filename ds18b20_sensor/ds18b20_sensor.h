f1b75a90b0ea5105391143e2fc11d2cd0f57a037
Brian Scully <brian@agponics.com>
2016-06-15 10:45:42 -0700

    Speed up ds18b20 temp readings

--- ds18b20_sensor/ds18b20_sensor.h
+++ ds18b20_sensor/ds18b20_sensor.h
@@ -21,18 +21,23 @@
 #define DS18B20_DBGMSG(msg)
 #endif
 
+#define MAX_PROBES 4 // for now enforce a limit for convenience, this can easily be changed later
+
 // a class to represent a DS18B20 sensor
 class CDs18b20Sensor : public CDevice
 {
 public: 
-    CDs18b20Sensor() : CDevice(), m_one_wire(0) {}
+    CDs18b20Sensor() : CDevice(), m_one_wire(0), m_device_cnt(0) {}
     void   set_pin(int pin); 
     String get_status_str();
-    String get_addrs();      // returns the addresses for all probes on the wire
+    String get_addrs_str();  // returns the addresses for all probes on the wire
 private:
     static double celsius_to_fahrenheit(double c) {return (c * (9.0/5.0) + 32.0);}
+    bool   cache_addrs();
     OneWire           m_one_wire;
     DallasTemperature m_dt;
+    uint8_t           m_addrs[MAX_PROBES][8]; // cache addresses for each probe to speed up readings
+    uint8_t           m_device_cnt;
 };
 
 #endif // __DS18B20_SENSOR_H__
